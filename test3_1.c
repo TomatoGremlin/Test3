fa5c09929212c15e0101bf8361680b4ebcf39d0f
TomatoGremlin <101596047+TomatoGremlin@users.noreply.github.com>
2022-08-08 12:38:22 +0300

    Update test3_1.c

--- test3_1.c
+++ test3_1.c
@@ -9,13 +9,13 @@ typedef struct Node {
 } Node;
 
 void print(Node *cll) { // pointer to first element
-    // curNode is used for iterating all elements
+  
     Node *curNode = cll;
-    //currentNode != NULL
+   
     while(curNode) {
         printf("addr: %p,  data: %ld,   next: %p\n",
             curNode, curNode->data, curNode->next);
-        // adress of next Node into curNode
+       
         curNode = curNode->next;
     }
 }
@@ -47,30 +47,26 @@ void push_back(Node *head, long new_data) {
         perror("Push_back: Could not malloc\n");
         exit(-1);
     }
-    // Set Node fields
+ 
     new_node -> data = new_data;
-    // new_node is going to be last, so new_node->next=NULL
+    
     new_node -> next = NULL;
  
-    // We need to attach new_node to linked list
-    // We must find the last node
+   
     Node *last_node = head;
-    // While loop ends when it finds Node that has no next Node
+    
     while(last_node->next != NULL) {
         last_node = last_node->next;
     }
-    // Attach new_node to last elements of linked list
+ 
     last_node -> next = new_node;
 }
 
 
 void pop_front(Node **head_pp) {
-    // Store deleted node in local temp variable
+
     Node *deleted_node = *head_pp;
  
-    // Change value of head pointer
-    // Point it to second element
-    // This is not possible without pointer to pointer   
     *head_pp = (*head_pp)->next;
  
     free(deleted_node);
@@ -100,6 +96,7 @@ int main() {
     for (size_t i = 0; i < n; i++)
     {
        scanf("%d ", numbers);
+        
        push_back(cll1, numbers);
     }
     
